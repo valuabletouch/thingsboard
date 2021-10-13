7bb01e531890cd6bda90b53e554646a74170b888
ozgunay41 <ozgun-41@hotmail.com>
2021-10-13 09:55:14 +0300

    Use VSensor Cassandra structure (read-only)

---
+++ dao/src/main/java/org/thingsboard/server/dao/nosql/mongo/ReadingTypeRepository.java
@@ -0,0 +1,15 @@
+/**
+ * Özgün AY
+ */
+package org.thingsboard.server.dao.nosql.mongo;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
+import org.thingsboard.server.dao.model.vsensor.ReadingType;
+
+@Service
+public interface ReadingTypeRepository extends MongoRepository<ReadingType, String> {
+    public Optional<ReadingType> findByCode(String code);
+}
